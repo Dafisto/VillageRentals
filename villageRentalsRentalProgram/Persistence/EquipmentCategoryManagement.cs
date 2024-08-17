a9aad93f8d851196602b9202ca22aa43a1bc28b2
CrusaderJohnny <17ark.leonardis@gmail.com>
2024-08-16 18:25:40 -0600

    admin finished

--- villageRentalsRentalProgram/Persistence/EquipmentCategoryManagement.cs
+++ villageRentalsRentalProgram/Persistence/EquipmentCategoryManagement.cs
@@ -21,15 +21,13 @@ namespace villageRentalsRentalProgram.Persistence
 
         public Task<int> InsertEquipmentCategory(EquipmentCategory category) // This will save the category 
         {
-            if (category.CategoryId != 0)
-            {
-                return connection.UpdateAsync(category);  //updates existing category at correct spot on table
-            }
-            else
-            {
-                return connection.InsertAsync(category); // will insert new category at end of table
-            }
+            return connection.InsertAsync(category); // will insert new category at end of table
         }
+        public Task<int> UpdateEquipmentCategory(EquipmentCategory category) // This will save the category 
+        {
+            return connection.UpdateAsync(category);  //updates existing category at correct spot on table
+        }
+
         public Task<List<EquipmentCategory>> GetAllEquipmentAsync() //returns list with all categories from the cateogory table
         {
             return connection.Table<EquipmentCategory>().ToListAsync();

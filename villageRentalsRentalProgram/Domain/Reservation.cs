0a75359c1b737df1a3e074d860748c75a2cf64bf
CrusaderJohnny <17ark.leonardis@gmail.com>
2024-08-16 21:20:41 -0600

    merged

--- villageRentalsRentalProgram/Domain/Reservation.cs
+++ villageRentalsRentalProgram/Domain/Reservation.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
-=======
 using villageRentalsRentalProgram.Persistence;
->>>>>>> master
 using SQLite;
 
 namespace villageRentalsRentalProgram.Domain
